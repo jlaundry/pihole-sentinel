2256e41e3fba224f26f44c4084fbdb09a9b09647
Jed Laundry <jlaundry@jlaundry.com>
2026-06-14 00:17:06 +0000

    update script for Logs Ingestion API

--- pihole-sentinel.py
+++ pihole-sentinel.py
@@ -1,16 +1,26 @@
 
 import logging
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.WARNING)
 
-from datetime import datetime
+from datetime import datetime, timezone
+import os
 import socket
 import sqlite3
 
-from azure_log_analytics import LogAnalytics
-from local_settings import AZURE_WORKSPACE_ID, AZURE_SECRET_KEY
+from azure.core.exceptions import HttpResponseError
+from azure.identity import DefaultAzureCredential
+from azure.monitor.ingestion import LogsIngestionClient
+
 
 DEVICE_HOSTNAME = socket.gethostname()
 DEVICE_IP6 = socket.getaddrinfo("www.google.com", 443, socket.AF_INET6)[0][4][0]
+
+DCE_ENDPOINT = os.environ['DATA_COLLECTION_ENDPOINT']
+DCR_RULE_ID = os.environ['LOGS_DCR_RULE_ID']
+DCR_STREAM_NAME = os.environ['LOGS_DCR_STREAM_NAME']
+
+CREDENTIAL = DefaultAzureCredential()
+
 
 def dict_factory(cursor, row):
     d = {}
@@ -60,7 +70,24 @@ QUERY_STATUS = {
     18: "Failure: By upstream server (EDE 15)"
 }
 
-sentinel = LogAnalytics(AZURE_WORKSPACE_ID, AZURE_SECRET_KEY)
+# https://docs.pi-hole.net/database/query-database/#supported-reply-types
+# https://www.iana.org/assignments/dns-parameters/dns-parameters.xhtml#dns-parameters-6
+REPLY_CODES = {
+    0:  (4095, "NA"),     # Unknown (no reply so far)
+    1:  (0, "NoError"),   # NODATA
+    2:  (3, "NXDomain"),  # NXDOMAIN
+    3:  (0, "NoError"),   # CNAME
+    4:  (0, "NoError"),   # IP
+    5:  (0, "NoError"),   # DOMAIN
+    6:  (0, "NoError"),   # RRNAME
+    7:  (2, "ServFail"),  # SERVFAIL
+    8:  (5, "Refused"),   # REFUSED
+    9:  (4, "NotImp"),    # NOTIMP
+    10: (4094, "NA"),     # OTHER
+    11: (4093, "NA"),     # DNSSEC
+    12: (5, "Refused"),   # NONE
+    13: (4091, "NA"),     # BLOB
+}
 
 last_filename = '.pihole-latest'
 LAST_ID = 0
@@ -74,7 +101,7 @@ except ValueError:
     pass
 
 now = datetime.now().isoformat()
-logging.info(f"Starting at {now} from queries.id={LAST_ID}")
+logging.warning(f"Starting at {now} from queries.id={LAST_ID}")
 
 def update_latest(rowid, force=False):
     global LAST_ID
@@ -87,24 +114,32 @@ def update_latest(rowid, force=False):
     LAST_ID = rowid
 
 
-#con = sqlite3.connect('/etc/pihole/pihole-FTL.db')
-con = sqlite3.connect('/tmp/pihole-FTL.db')
+con = sqlite3.connect("file:/etc/pihole/pihole-FTL.db?mode=ro", uri=True)
+
+logs = []
+
 con.row_factory = dict_factory
 cur = con.cursor()
-
 for row in cur.execute('SELECT * FROM queries WHERE id >:id ORDER BY id', {"id": LAST_ID}):
+    (event_result, event_message) = QUERY_STATUS.get(row['status'], "Unknown: Unknown").split(": ")
+    (rcode_value, rcode_text) = REPLY_CODES.get(row['reply_type'], (4090, "NA"))
+    timestamp = datetime.fromtimestamp(row['timestamp'], timezone.utc).isoformat().replace("+00:00", "Z")
 
     record = {
-        "TimeGenerated": datetime.utcfromtimestamp(row['timestamp']).isoformat() + "Z",
-
+        "TimeGenerated": timestamp,
         "EventCount": 1,
+        "EventStartTime": timestamp,
+        "EventEndTime": timestamp,
+        
         "EventOriginalUid": str(row['id']),
-        "EventType": "lookup",
-        "EventResult": QUERY_STATUS.get(row['status']).split(":")[0],
-        "EventResultDetails": QUERY_TYPES.get(row['type']),
-        "EventProduct": "Pi Hole",
-        "EventVendor": "Pi Hole",
-        "EventSchemaVersion": "0.1.1",
+        "EventType": "Query",
+        "EventResult": event_result,
+        "EventMessage": event_message,
+        "EventResultDetails": rcode_text,
+        "EventProduct": "Pi-hole",
+        "EventVendor": "Pi-hole",
+        "EventSchema": "Dns",
+        "EventSchemaVersion": "0.1.7",
         "Dvc": DEVICE_HOSTNAME,
         "DvcIpAddr": DEVICE_IP6,
         "DvcHostname": DEVICE_HOSTNAME,
@@ -112,14 +147,19 @@ for row in cur.execute('SELECT * FROM queries WHERE id >:id ORDER BY id', {"id":
         "SrcIpAddr": row['client'],
         "DnsQuery": row['domain'],
         "DnsQueryTypeName": QUERY_TYPES.get(row['type']),
-        "DnsResponseCodeName": "NA",
+        "DnsResponseCode": rcode_value,
     }
-
-    log_type = "Normalized"
-    logging.debug(record)
-    sentinel.post(log_type, record)
-    update_latest(row['id'])
-
+    logs.append(record)
 con.close()
-update_latest(row['id'], force=True)
 
+try:
+    if len(logs) > 0:
+        logs_ingestion_client = LogsIngestionClient(DCE_ENDPOINT, CREDENTIAL)
+        logs_ingestion_client.upload(rule_id=DCR_RULE_ID, stream_name=DCR_STREAM_NAME, logs=logs)
+        latest_id = int(logs[-1]['EventOriginalUid'])
+        update_latest(latest_id, force=True)
+        logging.warning(f"Uploaded {len(logs)} logs, up to queries.id={latest_id}")
+    else:
+        logging.warning("No new logs to upload.")
+except HttpResponseError as e:
+    logging.error(f"Upload failed: {e}")
